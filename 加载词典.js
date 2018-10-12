ac40637687d9b9317d1b6a61880041f3a404d758
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-10-12 10:36:20 -0700

    拆分词典数据

--- 加载词典.js
+++ 加载词典.js
@@ -1,10 +1,40 @@
-
-const 词典文件 = '词典数据/中文释义.json'
+const 词典路径 = '词典数据/测试'  // '词典数据/中文释义.json'
+const 文件扩展 = '.json'
+const 词典文件 = {"1": false, "2": false}
 var 词典数据 = {};
 
-fetch(chrome.runtime.getURL(词典文件))
+function 载入部分词典(文件) {
+  return function() {
+    var 文件路径 = 词典路径 + 文件 + 文件扩展;
+    console.log(文件路径)
+    fetch(chrome.runtime.getURL(文件路径))
   .then((响应) => 响应.json())
-  .then((数据) => 词典数据 = 数据);
+  .then((数据) => {
+    for (var 英文 in 数据) {
+      词典数据[英文] = 数据[英文];
+    }
+    词典文件[文件] = true
+    console.log(JSON.stringify(词典文件));
+    if (已载入词典()) {
+      console.log("已载入: " + ((new Date()) - 开始)/1000 + "秒");
+    }
+  });
+  }
+}
+
+var 开始 = new Date();
+for (var 文件 in 词典文件) {
+  载入部分词典(文件)();
+}
+
+function 已载入词典() {
+  for (var 文件 in 词典文件) {
+    if (!词典文件[文件]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function 取释义(选中文本) {
   var 释义 = 词典数据[选中文本];
--- 弹窗/主界面.js
+++ 弹窗/主界面.js
@@ -10,12 +10,21 @@ function 翻译选中文本() {
     // TODO: 如果没有toString, 报错: -,.toLowerCase is not function
     var 选中文本 = 选中内容.toString();
     if (!选中文本) {
-      document.body.innerHTML = "无选中文本";
+      置弹窗内容("无选中文本");
       return;
     }
-    var 释义 = chrome.extension.getBackgroundPage().取释义(选中文本);
-    document.body.innerHTML = 选中文本 + "<br/>" + (释义 ? 格式化(释义) : "未找到");
+    var 词典接口 = chrome.extension.getBackgroundPage();
+    if (!词典接口.已载入词典()) {
+      置弹窗内容("载入词典数据中...");
+      return;
+    }
+    var 释义 = 词典接口.取释义(选中文本);
+    置弹窗内容(选中文本 + "<br/>" + (释义 ? 格式化(释义) : "未找到"));
   });
+}
+
+function 置弹窗内容(文本) {
+  document.body.innerHTML = 文本;
 }
 
 翻译选中文本();
