b9a8fcf45390e445c96695e7c1cb9620ba640f78
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-10-12 11:34:33 -0700

    载入拆分词典

--- 加载词典.js
+++ 加载词典.js
@@ -1,19 +1,21 @@
-const 词典路径 = '词典数据/测试'  // '词典数据/中文释义.json'
+const 词典路径 = '词典数据/词典'
 const 文件扩展 = '.json'
-const 词典文件 = {"1": false, "2": false}
+var 词典文件 = {};
+for (var 文件序号 = 0; 文件序号<16; 文件序号++) {
+  词典文件[文件序号] = false;
+}
 var 词典数据 = {};
 
 function 载入部分词典(文件) {
   return function() {
     var 文件路径 = 词典路径 + 文件 + 文件扩展;
-    console.log(文件路径)
     fetch(chrome.runtime.getURL(文件路径))
   .then((响应) => 响应.json())
   .then((数据) => {
     for (var 英文 in 数据) {
       词典数据[英文] = 数据[英文];
     }
-    词典文件[文件] = true
+    词典文件[文件] = true;
     console.log(JSON.stringify(词典文件));
     if (已载入词典()) {
       console.log("已载入: " + ((new Date()) - 开始)/1000 + "秒");
