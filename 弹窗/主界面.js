7b8e7b802e5baf72d76ce75c3039c5708b4fc192
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-10-25 16:06:34 -0700

    #6 提供输入框, 回车查词

--- 弹窗/主界面.js
+++ 弹窗/主界面.js
@@ -8,8 +8,12 @@ function 翻译选中文本() {
       置弹窗内容("", "无选中文本");
       return;
     }
-    var 词典接口 = chrome.extension.getBackgroundPage();
 
+    查词并显示(选中文本);
+  });
+}
+
+function 查词并显示(选中文本) {
     // TODO: 似乎无效
     if (!词典接口.已载入词典()) {
       置弹窗内容("", "载入词典数据中...");
@@ -17,14 +21,12 @@ function 翻译选中文本() {
     }
     var 释义 = 词典接口.查词(选中文本);
     置弹窗内容(选中文本, 释义);
-  });
 }
 
 function 置弹窗内容(英文, 释义) {
   var 窗体 = document.body;
-  var 英文部分 = document.getElementById("英文");
-  var 释义部分 = document.getElementById("释义");
-  置文本内容(英文部分, 英文);
+  英文输入.value = 英文;
+  清除文本内容(释义部分);
   if (释义) {
     var 多行 = 释义.split('\\n');
     for (var 行数 in 多行) {
@@ -36,8 +38,24 @@ function 置弹窗内容(英文, 释义) {
   }
 }
 
+function 清除文本内容(元素) {
+  while (元素.hasChildNodes()) {
+    元素.removeChild(元素.firstChild)
+  }
+}
+
 function 置文本内容(元素, 文本) {
   元素.appendChild(document.createTextNode(文本));
 }
+
+var 词典接口 = chrome.extension.getBackgroundPage();
+var 释义部分 = document.getElementById("释义");
+var 英文输入 = document.getElementById("英文输入");
+
+英文输入.addEventListener('keypress', function(e){
+  if (e.keyCode == 13) {
+    查词并显示(英文输入.value);
+  }
+});
 
 翻译选中文本();
