a2c15ec35a2a41d0dea89befdafde6ca1df15422
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-10-25 23:04:15 -0700

    0.0.6 #5 修复外部接口

--- test/查词测试.js
+++ test/查词测试.js
@@ -1,6 +1,20 @@
 QUnit.test("英文带前后空格", function (assert) {
   assert.deepEqual(
     取释义({ "to": "到" }, ' to '),
-    {"英文": "to", "释义": "到"},
+    "到",
+    "");
+});
+
+QUnit.test("大写英文", function (assert) {
+  assert.deepEqual(
+    取释义({ "to": "到" }, 'To'),
+    undefined,
+    "");
+});
+
+QUnit.test("小写英文", function (assert) {
+  assert.deepEqual(
+    取释义({ "To": "到" }, 'to'),
+    undefined,
     "");
 });
